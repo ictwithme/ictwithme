f797621788d19d35ebad6b7510d1044b7d662daa
ictwithme <alizabiglari@gmail.com>
2025-06-28 22:44:23 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
-import asyncio
-import logging
 import os
-
+import asyncio
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -9,48 +7,32 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-from telegram import Update
-from telegram.constants import ParseMode
-from telegram.ext import ContextTypes
-
-from scheduler import create_scheduler
 from handlers import (
     start,
     handle_button_click,
     handle_text_message,
 )
+from scheduler import create_scheduler
+from dotenv import load_dotenv
 
-# بارگذاری توکن از متغیر محیطی
+load_dotenv()  # بارگذاری متغیرهای محیطی از .env
+
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# فعال‌سازی لاگ‌ها
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-
 async def main():
-    # ساخت اپلیکیشن تلگرام
+    # ساخت اپلیکیشن
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
-    # گرفتن شیء bot برای ارسال پیام از طریق زمان‌بند
-    bot = application.bot
-
-    # راه‌اندازی زمان‌بند برای بررسی اخبار فارکس
-    create_scheduler(bot)
-
-    # افزودن هندلرها
+    # اضافه کردن هندلرها
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(handle_button_click))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_message))
 
-    # اجرای ربات
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-    await application.idle()
+    # راه‌اندازی زمان‌بند
+    create_scheduler(application.bot)
 
+    # اجرای ربات با polling
+    await application.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
