4c1604074f31f03dfd4594bfcc11811ff2a901e1
ictwithme <alizabiglari@gmail.com>
2025-06-28 20:54:17 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 from dotenv import load_dotenv
+import pytz
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("7637175756:AAHU2LxBRVmOo7CbrIgl1j7ZovG3ex_En30")
-CHANNEL_USERNAME = os.getenv("@ictwithme")
-OPENAI_API_KEY = os.getenv("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")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@ictwithme")
+
+DB_PATH = "db.sqlite3"
+
+# منطقه زمانی نیویورک
+NEW_YORK_TIMEZONE = pytz.timezone("America/New_York")
