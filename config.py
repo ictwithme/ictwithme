165e075cf4f682c60a7af8bb9e52118361c1bf22
ictwithme <alizabiglari@gmail.com>
2025-06-28 20:31:50 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("7637175756:AAHU2LxBRVmOo7CbrIgl1j7ZovG3ex_En30")
+CHANNEL_USERNAME = os.getenv("@ictwithme")
+OPENAI_API_KEY = os.getenv("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")
